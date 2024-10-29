ded063ac1accc94dde217c8596ea23ed1ca9cab0
bewwww-1 <bewkugthailand@gmail.com>
2024-10-29 23:25:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function goToProductPage(productId) {
 document.getElementById('shopNowBtn').addEventListener('click', function() {
   window.location.href = 'product-category.html';
 });
+
+document.getElementById('loginBtn').addEventListener('click', function() {
+    window.location.href = 'login.html'; // เปลี่ยนไปยังหน้า login.html
+});
