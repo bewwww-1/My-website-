e57897fd453c8a604b6c7827413c54e4d6f028ad
bewwww-1 <bewkugthailand@gmail.com>
2024-10-29 16:36:25 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// ฟังก์ชันสำหรับปุ่ม "SHOP NOW"
+document.getElementById('shopNowBtn').addEventListener('click', function() {
+    alert('คุณได้คลิกที่ SHOP NOW!');
+});
+
+// ฟังก์ชันสำหรับปุ่มเข้าสู่ระบบ
+document.getElementById('loginBtn').addEventListener('click', function() {
+    alert('คุณได้คลิกที่ ลงชื่อเข้าใช้!');
+});
